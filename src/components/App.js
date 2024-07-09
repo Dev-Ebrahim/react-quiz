63e3fb9e03f02d6b3a508e93b4fbe890a861e4de
unknown <webdev.ebrahim@gmail.com>
2024-07-09 17:38:34 +0330

    complete project

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import { useEffect } from "react";
+
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+import { useQuiz } from "../Contexts/QuizContext";
+
+export default function App() {
+  const { questions, index, dispatch, status } = useQuiz();
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFaild" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
+        {status === "active" && (
+          <>
+            <Progress />
+            <Question question={questions[index]} />
+            <Footer>
+              <Timer />
+              <NextButton />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && <FinishScreen />}
+      </Main>
+    </div>
+  );
+}
---
+++ src/components/Progress.js
@@ -0,0 +1,22 @@
+import { useQuiz } from "../Contexts/QuizContext";
+
+function Progress() {
+  const { index, numQuestions, points, maxPossiblePoints, answer } = useQuiz();
+
+  return (
+    <header className="progress">
+      <progress
+        max={numQuestions}
+        value={index + Number(answer !== null)}
+      ></progress>
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints} points
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
